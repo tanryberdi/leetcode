8de9a257950dd68d4e68c2dd2ce3af630b51f735
tanryberdi <tanryberdi@gmail.com>
2022-11-19 15:13:21 +0300

    680-valid palindrome solved

---
+++ 680-valid-palindrome.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+func validPalindrome(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	tail := 0
+	head := len(s) - 1
+
+	// nod is a number of deleted characters from string
+	nod := 0
+
+	for {
+		//fmt.Println("Tail=", tail, " Head=", head)
+
+		if head < tail {
+			break
+		}
+
+		// if they are same characters
+		if s[tail] == s[head] {
+			tail++
+			head--
+			continue
+		} else {
+			if nod > 0 {
+				return false
+			}
+
+			if s[tail] == s[head-1] {
+				tail++
+				head -= 2
+				nod++
+				continue
+			}
+
+			if s[tail+1] == s[head] {
+				tail += 2
+				head--
+				nod++
+				continue
+			}
+
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	s := "ebcbbececabbacecbbcbe"
+	fmt.Println(validPalindrome(s))
+}
