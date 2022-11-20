0657fd5f0e152c25913ac4844499ef1b14beff06
tanryberdi <tanryberdi@gmail.com>
2022-11-20 23:38:35 +0300

    680-valid palindrome solved

--- 680-valid-palindrome.go
+++ 680-valid-palindrome.go
@@ -13,9 +13,8 @@ func validPalindrome(s string) bool {
 	// nod is a number of deleted characters from string
 	nod := 0
 
+	result := true
 	for {
-		//fmt.Println("Tail=", tail, " Head=", head)
-
 		if head < tail {
 			break
 		}
@@ -27,7 +26,12 @@ func validPalindrome(s string) bool {
 			continue
 		} else {
 			if nod > 0 {
-				return false
+				result = false
+				head = tail - 1
+			}
+
+			if head < tail {
+				break
 			}
 
 			if s[tail] == s[head-1] {
@@ -44,6 +48,50 @@ func validPalindrome(s string) bool {
 				continue
 			}
 
+			result = false
+			head = tail - 1
+		}
+	}
+
+	if result {
+		return true
+	}
+
+	tail = 0
+	head = len(s) - 1
+
+	// nod is a number of deleted characters from string
+	nod = 0
+
+	for {
+		if head < tail {
+			break
+		}
+
+		// if they are same characters
+		if s[tail] == s[head] {
+			tail++
+			head--
+			continue
+		} else {
+			if nod > 0 {
+				return false
+			}
+
+			if s[tail+1] == s[head] {
+				tail += 2
+				head--
+				nod++
+				continue
+			}
+
+			if s[tail] == s[head-1] {
+				tail++
+				head -= 2
+				nod++
+				continue
+			}
+
 			return false
 		}
 	}
@@ -52,6 +100,6 @@ func validPalindrome(s string) bool {
 }
 
 func main() {
-	s := "ebcbbececabbacecbbcbe"
+	s := "aeacdeaeaaaaaaeaedcae"
 	fmt.Println(validPalindrome(s))
 }
