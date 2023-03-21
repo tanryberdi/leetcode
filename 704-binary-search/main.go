23cf87173c620091d0982adbea62e1b52591bb11
Tanryberdi <tanryberdi@gmail.com>
2023-03-21 16:47:29 +0500

    704-binary-search solved

---
+++ 704-binary-search/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	nums := []int{-1, 0, 3, 5, 9, 12}
+	target := 9
+	fmt.Println(search(nums, target))
+}
