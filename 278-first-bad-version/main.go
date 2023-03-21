a7ed4373d9115f1957d106859323028cbd53d27e
Tanryberdi <tanryberdi@gmail.com>
2023-03-21 18:41:56 +0500

    278-first-bad-version is solved

---
+++ 278-first-bad-version/main.go
@@ -0,0 +1,23 @@
+package main
+
+func firstBadVersion(n int) int {
+	left := 1
+	right := n
+	for left < right {
+		mid := left + (right-left)/2
+		if isBadVersion(mid) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return left
+}
+func isBadVersion(version int) bool {
+	return false
+}
+
+func main() {
+
+}
