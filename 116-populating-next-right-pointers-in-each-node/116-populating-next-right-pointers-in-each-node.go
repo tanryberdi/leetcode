1d8ffce7a0c6b00fac06e295bc3c2dd19add8047
Tanryberdi <tanryberdi@gmail.com>
2023-03-28 18:24:13 +0500

    116-populating-next-pointers-in-each-node solved

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+	queue := []*Node{root}
+	for len(queue) > 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if i < size-1 {
+				node.Next = queue[0]
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+	return root
+}
+
+func main() {
+	root := &Node{
+		Val: 1,
+		Left: &Node{
+			Val: 2,
+			Left: &Node{
+				Val: 4,
+			},
+			Right: &Node{
+				Val: 5,
+			},
+		},
+		Right: &Node{
+			Val: 3,
+			Right: &Node{
+				Val: 7,
+			},
+		},
+	}
+	fmt.Println(connect(root))
+}
