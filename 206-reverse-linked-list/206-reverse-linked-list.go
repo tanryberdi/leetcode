3920123abdca385ff17418136cb253d102594de4
Tanryberdi <tanryberdi@gmail.com>
2023-03-30 10:59:40 +0500

    206-reverse-linked-list solved

---
+++ 206-reverse-linked-list/206-reverse-linked-list.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = prev
+		prev = head
+		head = next
+	}
+	return prev
+}
+
+func main() {
+	head := &ListNode{Val: 1}
+	fmt.Println(reverseList(head))
+}
