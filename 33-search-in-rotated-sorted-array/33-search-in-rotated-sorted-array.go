ba5b202c7b077b1a96d91d64665a794a0ee30f9c
Tanryberdi <tanryberdi@gmail.com>
2023-04-03 13:22:45 +0500

    33-search-in-rotated-sorted-array solved

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] >= nums[left] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
+func main() {
+	nums := []int{4, 5, 6, 7, 0, 1, 2}
+	target := 0
+	fmt.Println(search(nums, target))
+}
