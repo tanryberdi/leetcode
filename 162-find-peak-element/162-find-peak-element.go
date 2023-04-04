0dfb686b2327fffd8abaf333247d3071e8b53298
Tanryberdi <tanryberdi@gmail.com>
2023-04-04 10:14:33 +0500

    162-find-peak-element solved

---
+++ 162-find-peak-element/162-find-peak-element.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func findPeakElement(nums []int) int {
+	if len(nums) == 1 {
+		return 0
+	}
+	if nums[0] > nums[1] {
+		return 0
+	}
+	if nums[len(nums)-1] > nums[len(nums)-2] {
+		return len(nums) - 1
+	}
+	for i := 1; i < len(nums)-1; i++ {
+		if nums[i] > nums[i-1] && nums[i] > nums[i+1] {
+			return i
+		}
+	}
+	return 0
+}
+
+func main() {
+	nums := []int{1, 2, 3, 1}
+	fmt.Println(findPeakElement(nums))
+}
