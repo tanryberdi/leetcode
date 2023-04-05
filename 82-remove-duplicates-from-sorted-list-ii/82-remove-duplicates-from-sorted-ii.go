4bac0cfe237efbd0763e750cab4edc9508c59c9b
Tanryberdi <tanryberdi@gmail.com>
2023-04-05 10:36:57 +0500

    82-remove-duplicates-from-sorted-ii solved

---
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-ii.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	dummy := &ListNode{Next: head}
+	prev := dummy
+
+	for head != nil {
+		if head.Next != nil && head.Val == head.Next.Val {
+			for head.Next != nil && head.Val == head.Next.Val {
+				head = head.Next
+			}
+			prev.Next = head.Next
+		} else {
+			prev = prev.Next
+		}
+		head = head.Next
+	}
+
+	return dummy.Next
+}
+
+func main() {
+	head := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5}}}}}}}
+	fmt.Println(deleteDuplicates(head))
+}
