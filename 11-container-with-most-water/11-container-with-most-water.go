79e722ed95d51e30cae6e0e36d4862b09ff26e58
Tanryberdi <tanryberdi@gmail.com>
2023-04-06 14:03:59 +0500

    11-container-with-most-water solved

---
+++ 11-container-with-most-water/11-container-with-most-water.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func maxArea(height []int) int {
+	var res int
+	i, j := 0, len(height)-1
+	for i < j {
+		res = max(res, (j-i)*min(height[i], height[j]))
+		if height[i] < height[j] {
+			i++
+		} else {
+			j--
+		}
+	}
+	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	height := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+	fmt.Println(maxArea(height))
+}
