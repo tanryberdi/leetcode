3cfe833b172e8cbb6242097e825f2ddb74626d51
Tanryberdi <tanryberdi@gmail.com>
2023-04-17 14:03:54 +0500

    62-unique-paths solved

---
+++ 62-unique-paths/62-unique-paths.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	for i := 0; i < m; i++ {
+		dp[i][0] = 1
+	}
+	for i := 0; i < n; i++ {
+		dp[0][i] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
+
+func main() {
+	m := 3
+	n := 7
+	fmt.Println(uniquePaths(m, n))
+}
