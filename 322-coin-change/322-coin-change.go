55702c91222b2289fd7adb45df2ce29388f74a2a
Tanryberdi <tanryberdi@gmail.com>
2023-04-20 13:06:58 +0500

    322-coin-change solved

---
+++ 322-coin-change/322-coin-change.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	for i := 1; i <= amount; i++ {
+		dp[i] = amount + 1
+	}
+	for i := 1; i <= amount; i++ {
+		for _, coin := range coins {
+			if i >= coin {
+				dp[i] = min(dp[i], dp[i-coin]+1)
+			}
+		}
+	}
+	if dp[amount] > amount {
+		return -1
+	}
+	return dp[amount]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	coins := []int{1, 2, 5}
+	amount := 11
+	fmt.Println(coinChange(coins, amount))
+}
